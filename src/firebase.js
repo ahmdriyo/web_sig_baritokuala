ae8c8a66ab037d6119c14181919dccec0e2689c4
ahmdriyo <129720999+ahmdriyo@users.noreply.github.com>
2024-06-21 14:22:09 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage'; 
 import { getAuth } from 'firebase/auth';
 const firebaseConfig = {
-  apiKey: "AIzaSyBt4Q12r-ABBCk7XptnhTe68Uk9_qo9vHs",
-  authDomain: "web-barito.firebaseapp.com",
-  projectId: "web-barito",
-  storageBucket: "web-barito.appspot.com",
-  messagingSenderId: "221054702966",
-  appId: "1:221054702966:web:81bda3a6e87feca27e9657"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 const app = initializeApp(firebaseConfig);
